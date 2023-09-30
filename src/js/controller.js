ecfef83fe0f3b88332bf5c72dd2025b0a42006d0
30 <30@gmail.com>
2023-09-30 14:43:50 +0530

    changes into css

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,7 +8,21 @@ const showrecipe = async function () {
       "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
     );
     const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     console.log(res, data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceurl: recipe.source_url,
+      img: recipe.image_url,
+      serving: recipe.servings,
+      cookingtime: recipe.cooking_time,
+      ingredians: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
